a05c3838f543c998f5f77beca7e40b90f54d1f28
tomasruizt <tomas.ruiz.te@gmail.com>
2018-03-12 18:45:54 +0100

    The DiscreteState must be subclassed from now

--- rl/state/DiscreteState.py
+++ rl/state/DiscreteState.py
@@ -5,14 +5,15 @@ from rl.action import DiscreteAction
 
 class DiscreteState:
     """
-    Simple State with a discrete state representation and its corresponding action space.
+    Simple State with a discrete state representation and a corresponding
+    action space. This class in intended to be subclassed.
     """
 
-    def __init__(self, representation, action_space: Iterable[DiscreteAction]):
+    def __init__(self, action_space: Iterable[DiscreteAction]):
         """
-        Initializes a state with the given name and action space.
-        :param representation: Discrete representation of the state.
-        :param action_space: An iterable of actions that the agent can choose from.
+        Initializes a state with the given action space.
+        :param action_space: An iterable of actions that the agent can
+        choose from.
         """
-        self.representation = representation
         self.action_space = action_space
+        raise NotImplementedError

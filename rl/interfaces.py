affa8621e1044bec76caf45c999e3426bead1b57
tomasruizt <tomas.ruiz.te@gmail.com>
2018-03-12 14:43:38 +0100

    Add Pylint to improve standards

--- rl/interfaces.py
+++ rl/interfaces.py
@@ -1,4 +1,12 @@
+"""
+This is the module docstring
+"""
+
+
 class EnvironmentResponse:
+    """
+    This is a docstring
+    """
     def __init__(self, state, actions_available, reward):
         self.state = state
         self.actions_available = actions_available
@@ -6,18 +14,31 @@ class EnvironmentResponse:
 
 
 class Environment:
+    """The environment"""
     def get_response(self, action, current_state) -> EnvironmentResponse:
+        """
+        More docstring
+        :param action:
+        :param current_state:
+        :return:
+        """
         raise NotImplementedError
 
     def get_initial_state(self):
+        """
+        MOre docstring
+        :return:
+        """
         raise NotImplementedError
 
 
 class GameFinished(Exception):
+    """Docstring stuff"""
     pass
 
 
 class Policy:
+    """This is a policy"""
     def select_action(self, action_state_estimate_triplets):
         """
         Selects an appropriate action to take.
@@ -25,5 +46,4 @@ class Policy:
         (action, new_state, estimate) for the policy to choose from.
         :return: pair (idx, new_estimation)
         """
-        raise NotImplemented
-
+        raise NotImplementedError
